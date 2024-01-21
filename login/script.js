f98d62699645ce9cdbe17e9518eebaff5283a736
Haram Kwon <kharam1436@icloud.com>
2024-01-20 23:38:24 -0800

    placeholder not shirinking when there is an input

--- login/script.js
+++ login/script.js
@@ -1,3 +1,30 @@
+const emailField = document.querySelector("#username");
+const emailLabel = document.querySelector("#username-label");
+const passwordField = document.querySelector("#password");
+const passwordLabelId = document.querySelector("#password-label");
+
+const labelElementFocusOut = (inputElement, labelElement) => {
+  if (!!inputElement.value) {
+    labelElement.classList.remove("input-not-focused");
+    labelElement.classList.add("input-focused");
+  } else {
+    labelElement.classList.remove("input-focused");
+    labelElement.classList.add("input-not-focused");
+  }
+};
+
+emailField.addEventListener("focusout", () => {
+  labelElementFocusOut(emailField, emailLabel);
+});
+
+passwordField.addEventListener("focusout", () => {
+  labelElementFocusOut(passwordField, passwordLabelId);
+});
+
+passwordField.addEventListener("focusout", () => {
+  console.log("password");
+});
+
 window.addEventListener("load", () => {
   const searchParams = new URLSearchParams(window.location.search);
   const usernameField = document.querySelector("#username");
